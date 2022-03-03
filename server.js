b9a6a6055a6362860a782a3f1761e27d13b08d33
Tom Leysen <tom.leysen2002@gmail.com>
2022-03-03 19:46:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/angular-app-test'));
+app.use(express.static(__dirname + '/dist/photo-rc1'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/angular-app-test/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/photo-rc1/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 80);
