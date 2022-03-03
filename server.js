97763a7aa81b2f2ef2e46e3e2e286a4038582d95
Tom Leysen <tom.leysen2002@gmail.com>
2022-03-03 19:54:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/photo-rc1/index.html'));
 });
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 8080);
